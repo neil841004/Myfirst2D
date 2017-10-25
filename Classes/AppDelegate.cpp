cbec9df38cdc2c8da450a7bfac4261a8521b48bb
neil841004 <neil841004@gmail.com>
2017-10-25 16:31:34 +0800

    10/25

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,4 +1,4 @@
-﻿#define SceneTransition 1
+﻿#define SceneTransition 3
 
 #include "AppDelegate.h"
 #if SceneTransition == 1 || SceneTransition == 2
@@ -10,6 +10,7 @@
 USING_NS_CC;
 
 cocos2d::Size fSize;
+
 
 static cocos2d::Size screenResolutionSize = cocos2d::Size(1920, 1080);
 static cocos2d::Size designResolutionSize = cocos2d::Size(1280, 720);
@@ -82,6 +83,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
 	director->runWithScene(scene);
 #elif SceneTransition == 2
 	auto scene = TransitionMoveInL::create(0.6f, Scene101::createScene());
+	director->runWithScene(scene);
+#elif SceneTransition == 3
+	auto scene = TransitionZoomFlipX::create(0.6f, Scene102::createScene());
 	director->runWithScene(scene);
 #endif
 
